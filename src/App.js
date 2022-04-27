4eca4e9068b2ef3c68245c029d1c6d11b7156ff7
JR Pascua <jrpascua@gmail.com>
2022-04-26 21:54:19 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import '@aws-amplify/ui-react/styles.css'
+import { AmplifyProvider, withAuthenticator } from '@aws-amplify/ui-react'
+import PostCollection from './ui-components/PostCollection'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AmplifyProvider>
+      <PostCollection />
+    </AmplifyProvider>
+  )
 }
 
-export default App;
+export default withAuthenticator(App)
